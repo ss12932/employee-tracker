69d424a9cb832a58ac7f0ff4650db507960c4c9f
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-11 13:55:09 +0100

    database init + callback

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import mysql2 from 'mysql2';
 import consoleTable from 'console.table';
 import { Add, Delete, Update, View } from './lib';
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: '',
   database: 'ee_cms_db',
 });
+
+const errorCallback = (err) => {
+  if (err) throw err;
+  console.log('Welcome to the Employee Tracker CMS Application!');
+  init();
+};
+
+connection.connect(errorCallback);
