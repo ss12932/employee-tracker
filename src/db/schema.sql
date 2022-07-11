42bc211e34e87c08302253a3ac36d31a48de3270
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-11 13:42:28 +0100

    schema completed

---
+++ src/db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS ee_cms_db;
+CREATE DATABASE ee_cms_db;
+
+USE ee_cms_db;
+
+CREATE TABLE department (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import inquirer from 'inquirer';
 import mysql2 from 'mysql2';
 import consoleTable from 'console.table';
 import { Add, Delete, Update, View } from './lib';
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ee_cms_db',
+});
