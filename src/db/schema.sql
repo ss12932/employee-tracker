0113e1f96d655eaecc2e23c4c2163a0d73395ef8
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-03 17:22:35 +0100

    seed data

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE employee (
   id INTEGER NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL,
+  role_id INTEGER,
   manager_id INTEGER,
   PRIMARY KEY (id),
   FOREIGN KEY (role_id) REFERENCES role(id),
--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -2,7 +2,10 @@ INSERT INTO department (name)
 VALUES ("Finance"), ("Marketing"), ("Sales"), ("Operations"), ("Research & Development"),("Human Resources");
 
 INSERT INTO role (title, salary, department_id) 
-VALUES ("Accountant", "50000", 1), ("Accounting Manager", "75000", 1), ("Auditor", '45000', 1), ("Tax Specialist", "40000", 1), ("Marketing Analyst", "50000", 2), ("Marketing Director", "75000", 2), ("Marketing Consultant", '45000', 2), ("Social Media Promoter", "40000", 2), ("Sales Manager", "150000", 3), ("Sales Analyst", "55000", 3), ("Sales Consultant", '45000', 3), ("Sales Assistant", "40000", 3), ("Operations Officer", '45000', 4), ("Operations Manager", "140000", 4), ("R&D Manager", '145000', 5), ("R&D Researcher", "100000", 5), ("Head Recruiter", '65000', 6), ("Human Resources Administrator", "50000", 6);
-
+VALUES ("Accountant", "50000", 1), ("Accounting Manager", "75000", 1), ("Auditor", '45000', 1), ("Tax Specialist", "40000", 1), ("Marketing Analyst", "50000", 2), ("Marketing Director", "75000", 2), ("Marketing Consultant", '45000', 2), ("Social Media Promoter", "40000", 2), ("Sales Manager", "150000", 3), ("Sales Analyst", "55000", 3), ("Sales Consultant", '45000', 3), ("Sales Assistant", "40000", 3), ("Operations Officer", '45000', 4), ("Operations Manager", "140000", 4), ("R&D Manager", '145000', 5), ("R&D Researcher", "100000", 5), ("Head Recruiter", '65000', 6), ("HR Admin", "50000", 6);
+-- Insert Managers first
+INSERT employee (first_name, last_name, role_id)
+VALUES ("Peter", "Doe", 2), ("Jerry", "Loughborough", 6), ("Heidi", "Patel", 9), ("Danielle", "Collingwood", 14), ("Jie", "Xixin", 15), ("Michelle", "Watson", 17);
+-- Insert employees second
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 2), ("Peter", "Doe", 2, null), ("Sandra", "Williams", 3, 2), ("Tony", "Stark", 4, 2), ("Michael", "Berry", 5, 6), ("Jerry", "Loughborough", 6, null), ("Emma", "Headley", 7, 6), ("Jack", "Frost", 8, 6), ("Heidi", "Patel", 9, null), ("Jack", "Dean", 10, 9), ("Sarah", "Loomley", 11, 9), ("Jackie", "Nickels", 12, 9), ("Martin", "Wood", 13, 14), ("Danielle", "Collingwood", 14, null), ("Jie", "Xixin", 15, null), ("Danny", "Lee", 16, 15), ("Michelle", "Watson", 17, null), ("Tom", "Reynolds", 18, 17);
+VALUES ("John", "Doe", 1, 1), ("Sandra", "Williams", 3, 1), ("Tony", "Stark", 4, 1), ("Michael", "Berry", 5, 2), ("Emma", "Headley", 7, 2), ("Jack", "Frost", 8, 2), ("Jack", "Dean", 10, 3), ("Sarah", "Loomley", 11, 3), ("Jackie", "Nickels", 12, 3), ("Martin", "Wood", 13, 4), ("Danny", "Lee", 16, 5), ("Tom", "Reynolds", 18, 6);
