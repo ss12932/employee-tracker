ad7de48437b7b7c207a0d50c387f443f4e7f7cba
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-04 00:32:29 +0100

    viewEmployeesByDepartment done and tested in cli

--- src/lib/view.js
+++ src/lib/view.js
@@ -1,6 +1,4 @@
 import inquirer from 'inquirer';
-import consoleTable from 'console.table';
-
 class View {
   constructor(connection, callback) {
     this.connection = connection;
@@ -15,7 +13,39 @@ class View {
     });
   }
 
-  viewEmployeesByDept() {}
+  viewEmployeesByDept() {
+    this.connection.query(
+      'SELECT department.id, department.name AS department FROM department',
+      async (error, results) => {
+        // array of objects
+        // console.log(results);
+        if (error) throw error;
+        const choice = await inquirer.prompt([
+          {
+            name: 'department_id',
+            type: 'list',
+            message: 'Please select one of the following departments:',
+            choices: function () {
+              // map will return array of objects here which will provide us for choice property in inquirer prompt
+              return results.map((choice) => ({
+                name: `${choice.department}`,
+                value: choice.id,
+              }));
+            },
+          },
+        ]);
+        console.log(choice);
+        this.connection.query(
+          `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id WHERE department.id = ${choice.department_id}`,
+          (error, results) => {
+            if (error) throw error;
+            console.table(results);
+            this.callback();
+          }
+        );
+      }
+    );
+  }
   viewAllRoles() {
     this.connection.query('SELECT * FROM role', (error, results) => {
       if (error) throw error;
@@ -35,8 +65,10 @@ class View {
   }
   viewEmployeesByManager() {
     this.connection.query(
-      'SELECT employee.id, CONCAT(employee.first_name, " ", employee.last_name) AS manager FROM employee WHERE employee.manager_id IS NULL',
+      'SELECT employee.id, CONCAT(employee.first_name, " ", employee.last_name) AS manager, department.name AS department  FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id WHERE employee.manager_id IS NULL',
       async (error, results) => {
+        // array of objects
+        // console.log(results);
         if (error) throw error;
         const choice = await inquirer.prompt([
           {
@@ -44,8 +76,9 @@ class View {
             type: 'list',
             message: 'Please select one of the following managers:',
             choices: function () {
+              // map will return array of objects here which will provide us for choice property in inquirer prompt
               return results.map((choice) => ({
-                name: choice.manager,
+                name: `${choice.manager} || Manager of ${choice.department}`,
                 value: choice.id,
               }));
             },
