0e2785b59385ee61e3e149ff21fffa7cfe6ff068
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-04 00:33:32 +0100

    remove add.js query

--- src/lib/add.js
+++ src/lib/add.js
@@ -5,15 +5,6 @@ class Add {
   }
   addDepartment() {}
   addRole() {}
-  addEmployee() {
-    this.connection.query(
-      'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id',
-      (error, results) => {
-        if (error) throw error;
-        console.table(results);
-        this.callback();
-      }
-    );
-  }
+  addEmployee() {}
 }
 export default Add;
