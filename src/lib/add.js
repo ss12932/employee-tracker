d11e4157a79796ce55a7aec893d09c55f8b51ae5
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-06 03:41:12 +0100

    finish add class

--- src/lib/add.js
+++ src/lib/add.js
@@ -83,6 +83,74 @@ class Add {
       }
     );
   }
-  addEmployee() {}
+  addEmployee() {
+    this.connection.query(
+      `SELECT role.*, employee.manager_id, CONCAT(employee.first_name, " ", employee.last_name) AS manager, department.name as department FROM role LEFT JOIN employee ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id`,
+      async (error, results) => {
+        console.log(results);
+        if (error) throw error;
+        const addEmployee = await inquirer.prompt([
+          {
+            type: 'input',
+            name: 'firstName',
+            message: "What is the employee's first name?",
+            validate: (answer) => {
+              if (!answer) {
+                return 'Please enter the first name of the employee you would like to add.';
+              }
+              return true;
+            },
+          },
+          {
+            type: 'input',
+            name: 'lastName',
+            message: (answer) => `What is ${answer.firstName}'s last name?`,
+            validate: (answer) => {
+              if (!answer) {
+                return 'Please enter the last name of the employee you would like to add.';
+              }
+              return true;
+            },
+          },
+          {
+            type: 'list',
+            name: 'role',
+            message: (answer) =>
+              `What is ${answer.firstName} ${answer.lastName}'s role at the company?`,
+            choices: function () {
+              return results
+                .filter((value, index, self) => {
+                  return (
+                    self.findIndex((v) => v.title === value.title) === index
+                  );
+                })
+                .map((choice) => ({
+                  name: `${choice.title}`,
+                  value: {
+                    role_id: `${choice.id}`,
+                    title: `${choice.title}`,
+                    manager_id: `${choice.manager_id}`,
+                    manager: `${choice.manager}`,
+                    department: `${choice.department}`,
+                    salary: `${choice.salary}`,
+                  },
+                }));
+            },
+          },
+        ]);
+
+        this.connection.query(
+          `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${addEmployee.firstName}", "${addEmployee.lastName}", "${addEmployee.role.role_id}", ${addEmployee.role.manager_id});`,
+          (error, results) => {
+            if (error) throw error;
+            console.log(
+              `${addEmployee.firstName} ${addEmployee.lastName} has been added with role: ${addEmployee.role.title}, with salary: £${addEmployee.role.salary}, under the supervision of ${addEmployee.role.manager} in the ${addEmployee.role.department} department.`
+            );
+            this.callback();
+          }
+        );
+      }
+    );
+  }
 }
 export default Add;
