3401ce208fa7ca47da7306e0940a1edfc4a9628f
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-09 02:42:57 +0100

    update class finished finally...

--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -2,7 +2,7 @@ INSERT INTO department (name)
 VALUES ("Finance"), ("Marketing"), ("Sales"), ("Operations"), ("Research & Development"),("Human Resources");
 
 INSERT INTO role (title, salary, department_id) 
-VALUES ("Accountant", "50000", 1), ("Accounting Manager", "75000", 1), ("Auditor", '45000', 1), ("Tax Specialist", "40000", 1), ("Marketing Analyst", "50000", 2), ("Marketing Director", "75000", 2), ("Marketing Consultant", '45000', 2), ("Social Media Promoter", "40000", 2), ("Sales Manager", "150000", 3), ("Sales Analyst", "55000", 3), ("Sales Consultant", '45000', 3), ("Sales Assistant", "40000", 3), ("Operations Officer", '45000', 4), ("Operations Manager", "140000", 4), ("R&D Manager", '145000', 5), ("R&D Researcher", "100000", 5), ("Head Recruiter", '65000', 6), ("HR Admin", "50000", 6);
+VALUES ("Accountant", "55000", 1), ("Accounting Manager", "75000", 1), ("Auditor", '45000', 1), ("Tax Specialist", "43000", 1), ("Marketing Analyst", "52000", 2), ("Marketing Director", "75000", 2), ("Marketing Consultant", '45000', 2), ("Social Media Promoter", "41000", 2), ("Sales Manager", "150000", 3), ("Sales Analyst", "56000", 3), ("Sales Consultant", '45000', 3), ("Sales Assistant", "47000", 3), ("Operations Officer", '41000', 4), ("Operations Manager", "140000", 4), ("R&D Manager", '145000', 5), ("R&D Researcher", "100000", 5), ("Head Recruiter", '65000', 6), ("HR Admin", "50000", 6);
 -- Insert Managers first
 INSERT employee (first_name, last_name, role_id)
 VALUES ("Peter", "Doe", 2), ("Jerry", "Loughborough", 6), ("Heidi", "Patel", 9), ("Danielle", "Collingwood", 14), ("Jie", "Xixin", 15), ("Michelle", "Watson", 17);
--- src/lib/update.js
+++ src/lib/update.js
@@ -1,9 +1,118 @@
+import inquirer from 'inquirer';
+
 class Update {
   constructor(connection, callback) {
     this.connection = connection;
     this.callback = callback;
   }
-  updateEmployeeRole() {}
-  updateEmployeeManagers() {}
+  updateEmployeeRole() {
+    this.connection.query(
+      `SELECT e.id, e.first_name, e.last_name, r.id AS role_id, r.title, d.name as department FROM employee AS e LEFT JOIN role AS r ON r.id = e.role_id LEFT JOIN department AS d ON d.id = r.department_id;`,
+      async (error, results) => {
+        console.log(results);
+        if (error) throw error;
+        const updateRole = await inquirer.prompt([
+          {
+            type: 'list',
+            name: 'oldRole',
+            message: 'Which employee do you want to reassign role?',
+            choices: function (choice) {
+              return results.map((choice) => ({
+                name: `${choice.first_name} ${choice.last_name} (${choice.department})`,
+                value: {
+                  employee_id: choice.id,
+                  first_name: choice.first_name,
+                  last_name: choice.last_name,
+                  department: choice.department,
+                  old_role: choice.title,
+                },
+              }));
+            },
+          },
+          {
+            type: 'list',
+            name: 'newRole',
+            message: 'Which role do you want to assign to this employee?',
+            choices: function (choice) {
+              return results.map((choice) => ({
+                name: `${choice.title} (${choice.department})`,
+                value: {
+                  new_role_id: choice.role_id,
+                  title: choice.title,
+                  new_dpt: choice.department,
+                },
+              }));
+            },
+          },
+        ]);
+
+        this.connection.query(
+          `UPDATE employee SET role_id = "${updateRole.newRole.new_role_id}" WHERE id = "${updateRole.oldRole.employee_id}";`,
+          (error, results) => {
+            console.log(results);
+            console.log(
+              `${updateRole.oldRole.first_name} ${updateRole.oldRole.last_name} has successfully been reassigned a new role from ${updateRole.oldRole.old_role} (${updateRole.oldRole.department}) to ${updateRole.newRole.title} (${updateRole.newRole.new_dpt}).`
+            );
+          }
+        );
+        this.callback();
+      }
+    );
+  }
+  updateEmployeeManagers() {
+    this.connection.query(
+      `SELECT e.id, e.first_name, e.last_name, CONCAT(e.first_name, " ", e.last_name) AS manager, e.manager_id, r.id AS role_id, r.title, d.name as department FROM employee AS e LEFT JOIN role AS r ON r.id = e.role_id LEFT JOIN department AS d ON d.id = r.department_id LEFT JOIN employee AS manager ON e.manager_id = manager.id;`,
+      async (error, results) => {
+        console.log(results);
+        if (error) throw error;
+        const updateManager = await inquirer.prompt([
+          {
+            type: 'list',
+            name: 'oldManager',
+            message: 'Which employee do you want to reassign manager?',
+            choices: function (choice) {
+              return results.map((choice) => ({
+                name: `${choice.first_name} ${choice.last_name} (${choice.department})`,
+                value: {
+                  employee_id: choice.id,
+                  first_name: choice.first_name,
+                  last_name: choice.last_name,
+                  department: choice.department,
+                  old_manager: choice.manager,
+                },
+              }));
+            },
+          },
+          {
+            type: 'list',
+            name: 'newManager',
+            message: 'Which manager do you want to assign to this employee?',
+            choices: function (choice) {
+              return results.map((choice) => ({
+                name: `${choice.manager} (${choice.department})`,
+                value: {
+                  manager_emp_id: choice.id,
+                  new_manager: choice.manager,
+                  new_dpt: choice.department,
+                },
+              }));
+            },
+          },
+        ]);
+
+        this.connection.query(
+          `UPDATE employee SET manager_id = ${updateManager.newManager.manager_emp_id} WHERE id = ${updateManager.oldManager.employee_id};`,
+          (error, results) => {
+            console.log(results);
+            console.table(results);
+            console.log(
+              `${updateManager.oldManager.first_name} ${updateManager.oldManager.last_name} has successfully been reassigned a new manager from ${updateManager.oldManager.old_manager} (manager of ${updateManager.oldManager.department}) to ${updateManager.newManager.new_manager} (manager of ${updateManager.newManager.new_dpt}).`
+            );
+          }
+        );
+        this.callback();
+      }
+    );
+  }
 }
 export default Update;
